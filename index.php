25328c199b53e6f2e3e21e76965c131ed605a909
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-30 09:07:00 +0200

    exo8p5 php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/*Création du tableau avec les douze mois en Anglais*/
+  $months = array ('January','February','march','april','may','june','july','august','september','october','november','december');
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 8</title>
+  </head>
+  <body>
+    <p>
+<?php
+  for($number = 0; $number < 12; $number++){
+    echo $months[$number] . '<br />';
+  }
+?>
+    </p>
+  </body>
+</html>
